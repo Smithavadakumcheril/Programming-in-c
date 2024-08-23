c7a5d34c663a48bbbcf4c92a6a0fb4883f83b2d9
Smithavadakumcheril <Smithavadakumcheril23@gmail.com>
2024-08-23 23:52:00 +0530

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,58 +1,58 @@
-#include<stdio.h>
-void main()
-{
-int i,n,mid,x,left=0,right,a[100],flag=0,t=0;
-t++;
-t++;
-t++;
-printf("enter the size of an array");
-t++;
-scanf("%d",&n);
-t++;
-printf("enter the %d integers \n",n);
-t++;
-for(i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-t++;
-}
-right=(n-1);
-t++;
-printf("enter number to be found");
-t++;
-scanf("%d",&x);
-t++;
-while(left<=right)
-{
-mid=(left+right)/2;
-if(x==a[mid])
-{
-flag=1;
-break;
-}
-else if(x>a[mid])
-{
-left=mid+1;
-continue;
-}
-}
-t++;
-t++;
-t++;
-t++;
-if(flag==1)
-{
-printf("element is found \n");
-}
-else if(flag == 0)
-{
-printf("element not found \n");
-t++;
-}
-t++;
-printf("space complexity=%d\n",(8*4)+4*n);
-t++;
-t++;
-printf("time complexity=%d\n",t);
-}
 
+#include<stdio.h>
+int main() {
+
+    int  i, n, x, flag = 0, time=0;
+    time++;
+
+    printf("Enter the number of elements: ");
+    time++;
+    scanf("%d", &n);
+    time++;
+
+    int a[n], mid, left = 0, right = n - 1;
+    time++; time++;
+
+    printf("Enter the elements: ");
+    time++;
+
+    for (i = 0;i < n;i++) {
+        time++;
+        scanf("%d", &a[i]);
+        time++;
+    }
+
+    printf("Enter the element to be searched: ");
+    time++;
+    scanf("%d", &x);
+    time++;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+        time++;
+
+        if (x == a[mid]) {
+            flag = 1;
+            break;
+        }
+        else if (x > a[mid]) {
+            left = mid + 1;
+            time++;
+        }
+        else if (x < a[mid]) {
+            right = mid - 1;
+            time++;
+        }
+    }
+    if (flag == 0) {
+        printf("Element not found\n");
+        time++;
+    }
+    else if (flag == 1) {
+        printf("Element found at location %d\n", mid + 1);
+        time++;
+    }
+
+    time++;
+    printf("Space complexity = %d\nTime complexity = %d\n", 32 + (4 * n), time);
+}
